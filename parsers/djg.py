e510b7a405342b56fdba670fa0786b71625f43d2
nrapanos <nrapanos@gmail.com>
2020-08-13 10:19:45 +0300

    uploading data

--- parsers/djg.py
+++ parsers/djg.py
@@ -1,3 +1,5 @@
-import f90nml
+import xml.etree.ElementTree as ET
 
-nml = f90nml.read(r'C:\Users\nrapa\git\nlp-hft\data\DowJones_Newswires\sample\DJG-US-20200323-20200330\2020-03-24.nml')
+filepath = r'C:\Users\nrapa\git\nlp-hft\data\DowJones_Newswires\sample\DJG-US-20200323-20200330\test.nml'
+tree = ET.parse(filepath)
+root = tree.getroot()
